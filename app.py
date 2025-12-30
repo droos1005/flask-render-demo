4e9358ae35d6592792bc5f86b02928b46f8d3206
Gish Lee <droos1005@gmail.com>
2025-12-30 15:44:15 +0800

    change message to Hello Flask3

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 from flask import Flask
-app=Flask(__name__) # __name__代表目前執行的模組
 
-@app.route("/") # 函式的裝飾 (Decotate): 以函式為基礎, 附加提供的功能
+app = Flask(__name__)
+
+@app.route("/")
 def home():
-    return "Hello FLask2"
-
-@app.route("/test") # 網址後加上 /test
-def test():
-    return "This is test"
-
-if __name__=="__main__": # 如果以主程式運作
-    app.run() # 立刻啟動伺服器w
+    return "Hello Flask3"
